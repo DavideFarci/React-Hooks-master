7027b2a9d9c8a6a704482ffd223a6144c19c324e
DavideFarci <yank92.df@gmail.com>
2023-10-10 12:05:11 +0200

    1-useState, 4-useState.object

--- src/lezioni/1-useState/inizio/4-useState.object.js
+++ src/lezioni/1-useState/inizio/4-useState.object.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const StateObject = () => {
-  return <div>Object Componente UseState</div>;
+  // const [name, setName] = useState("Dario");
+  // const [age, setAge] = useState(24);
+  // const [saluto, setSaluto] = useState("Ciao da Dario!");
+  const [person, setPerson] = useState({
+    name: "dario",
+    age: 24,
+    saluto: "ciao",
+  });
+
+  const cambiaSaluto = () => {
+    setPerson({
+      ...person,
+      age: 25,
+      saluto: "Ciao oggi è il mio compleanno!",
+    });
+    // setAge(25);
+    // setSaluto("Ciao oggi è il mio compleanno!");
+  };
+
+  return (
+    <div className="item shadow">
+      <div className="text-left">
+        <h5>{person.name}</h5>
+        <h5>{person.age}</h5>
+        <h6>{person.saluto}</h6>
+        <button className="button" onClick={cambiaSaluto}>
+          Cambia il saluto
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default StateObject;
