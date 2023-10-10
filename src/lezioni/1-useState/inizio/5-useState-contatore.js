30ca635272fbd58a55e6267a3e4416c6b412545e
DavideFarci <yank92.df@gmail.com>
2023-10-10 12:23:29 +0200

    1-useState, 5-contatore

--- src/lezioni/1-useState/inizio/5-useState-contatore.js
+++ src/lezioni/1-useState/inizio/5-useState-contatore.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CounterComponent = () => {
+  const [contatore, setContatore] = useState(0);
+
+  const reset = () => {
+    setContatore(0);
+  };
+
+  const aumenta = () => {
+    // setContatore prende un parametro che rappresenta lo stato del contatore prima che questo venga modificato, ed è un metodo funzionale che deve ritornare qualcosa
+    setContatore((oldValue) => {
+      if (oldValue + 1 === 5) {
+        return oldValue;
+      }
+      return oldValue + 1;
+    });
+  };
+
   return (
-    <div>
-      <h3> Conter Component</h3>
+    <div className="bg-white shadow rounded py-5 w-75 col-6 offset-3 p-3">
+      <h3> {contatore} </h3>
+      <div className="d-flex justify-content-between">
+        <button
+          className="button my-2"
+          onClick={() => setContatore(contatore - 1)}
+        >
+          Diminuisci
+        </button>
+        <button className="button my-2" onClick={aumenta}>
+          Aumenta
+        </button>
+        <button className="button reset-button my-2" onClick={reset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 };
