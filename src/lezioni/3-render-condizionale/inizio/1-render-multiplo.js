f90b16f4b4f2da9507dab2bb32ba044adad50934
DavideFarci <yank92.df@gmail.com>
2023-10-14 10:29:12 +0200

    2-render-condizionale, 1-render-multiplo

--- src/lezioni/3-render-condizionale/inizio/1-render-multiplo.js
+++ src/lezioni/3-render-condizionale/inizio/1-render-multiplo.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const url = "https://api.github.com/users/QuincyLarson";
 
 const ConditionalCompining = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [user, setUser] = useState("Default user");
+
+  const getData = async () => {
+    setError(false);
+    setLoading(true);
+    try {
+      const response = await axios.get(url);
+      const { login } = response.data;
+      setUser(login);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <ErrorComponent />;
+  }
+
   return (
     <div>
-      <h2>Conditional Rendering</h2>
+      <h2>{user}</h2>
+    </div>
+  );
+};
+
+const Loading = () => {
+  return (
+    <div>
+      <h2> Loading... </h2>
+    </div>
+  );
+};
+
+const ErrorComponent = () => {
+  return (
+    <div>
+      <h2> Sorry, there is an Error!</h2>
     </div>
   );
 };
