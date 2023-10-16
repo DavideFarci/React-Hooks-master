7c7a48e8f417a06e99dd3ca47efdaefd5526adda
DavideFarci <yank92.df@gmail.com>
2023-10-16 11:12:28 +0200

    3-render-condizionale, 2-render-short-circuit

--- src/lezioni/3-render-condizionale/inizio/2-render-short-circuit.js
+++ src/lezioni/3-render-condizionale/inizio/2-render-short-circuit.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShortRender = () => {
+  const [parola, setParola] = useState("Valore");
+
+  // Shor circuit evaluaation con or operator
+  const esempio = parola || "sono un esempio";
+
+  // Short circuit evaluation with and (&&) operator
+  const esempio2 = parola && "ho un valore";
+
   return (
     <div>
-      <h2>short render Component</h2>
+      <h2>{esempio}</h2>
+      {parola && <h4>Ciao sono un componente</h4>}
     </div>
   );
 };
